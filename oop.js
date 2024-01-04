e44e6f3c1eac354eb7ce76e2fe8addf8de8f72fe
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2024-01-04 21:14:30 +0100

    new script instruction

--- oop.js
+++ oop.js
@@ -2,6 +2,7 @@ class ClassWithStaticMethod {
     static staticProperty = 'someValue';
     static staticMethod() {
       return 'static method has been called.';
+      return 'static method has been called.';
     }
     static {
       console.log('Class static initialization block called');
